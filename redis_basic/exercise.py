767d95876915fc5076f481f2c63294446800d5cb
jrosmanov <cr.osmanov@gmail.com>
2026-03-17 12:20:49 +0400

    task

--- redis_basic/exercise.py
+++ redis_basic/exercise.py
@@ -7,10 +7,12 @@ from typing import Union
 
 class Cache:
     def __init__(self) -> None:
+        """Redis connection qurur"""
         self._redis = redis.Redis()
         self._redis.flushdb()
 
     def store(self, data: Union[str, bytes, int, float]) -> str:
+        """verilenleri yaddasda saxlayir ve qaytarir"""
         random_key = str(uuid.uuid4())
         self._redis.set(random_key, data)
         return random_key
