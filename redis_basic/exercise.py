f1f416b81198ad02aed0a988a3901c0ff5347a7c
jrosmanov <cr.osmanov@gmail.com>
2026-03-17 12:47:10 +0400

    task

--- redis_basic/exercise.py
+++ redis_basic/exercise.py
@@ -1,18 +1,38 @@
 #!/usr/bin/env python3
-"""Redis basic module"""
+"""
+Redis ilə işləmək üçün Cache klassı.
+"""
 import redis
 import uuid
 from typing import Union
 
 
 class Cache:
-    def __init__(self) -> None:
-        """creating redis for data"""
+    """
+    Məlumatları Redis-də saxlamaq üçün keş sistemi.
+    """
+
+    def __init__(self):
+        """
+        Redis müştərisini (client) yaradır və verilənlər bazasını təmizləyir.
+        """
         self._redis = redis.Redis()
         self._redis.flushdb()
 
     def store(self, data: Union[str, bytes, int, float]) -> str:
-        """storing data in redis"""
+        """
+        Daxil edilən məlumatı təsadüfi bir key ilə Redis-də saxlayır.
+
+        Args:
+            data: Saxlanılacaq məlumat (str, bytes, int və ya float).
+
+        Returns:
+            Generasiya olunmuş random key (string).
+        """
+        # Təsadüfi ID (key) yaradırıq
         random_key = str(uuid.uuid4())
+        
+        # Məlumatı Redis-də saxlayırıq
         self._redis.set(random_key, data)
+        
         return random_key
