5774565ae616c7abb4e6a17d62188aaead2d3794
jrosmanov <cr.osmanov@gmail.com>
2026-03-17 12:19:03 +0400

    task

---
+++ redis_basic/exercise.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+"""Redis basic tapşırığı: Cache klassı və store metodu"""
+import redis
+import uuid
+from typing import Union
+
+
+class Cache:
+    def __init__(self) -> None:
+        self._redis = redis.Redis()
+        self._redis.flushdb()
+
+    def store(self, data: Union[str, bytes, int, float]) -> str:
+        random_key = str(uuid.uuid4())
+        self._redis.set(random_key, data)
+        return random_key
