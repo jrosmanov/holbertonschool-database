3ffc482f9860bde52ef5be002f37e73a2df5df32
jrosmanov <cr.osmanov@gmail.com>
2026-03-21 13:05:11 +0400

    task

--- redis_basic/exercise.py
+++ redis_basic/exercise.py
@@ -16,29 +16,3 @@ class Cache:
         random_key = str(uuid.uuid4())
         self._redis.set(random_key, data)
         return random_key
-    
-    def get(self, key: str, fn=None) -> Union[str, bytes, int, float]:
-        """getting data from redis"""
-        value = self._redis.get(key)
-        if value is None:
-            return None
-        if fn is not None:
-            return fn(value)
-        return value
-    
-    def get_str(self, key: str) -> str:
-        """getting string data from redis"""
-        value = self._redis.get(key)
-        if value is None:
-            return None
-        return value.decode('utf-8')
-    
-    def get_int(self, key: str) -> int:
-        """getting integer data from redis"""
-        value = self._redis.get(key)
-        if value is None:
-            return None
-        try:
-            return int(value)
-        except ValueError:
-            return None
