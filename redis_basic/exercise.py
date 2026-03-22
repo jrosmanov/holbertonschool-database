ce66a92c82e669ea82528eecb6f385f3b23f20bd
jrosmanov <cr.osmanov@gmail.com>
2026-03-22 13:43:09 +0400

    task

--- redis_basic/exercise.py
+++ redis_basic/exercise.py
@@ -2,7 +2,7 @@
 """Redis basic module"""
 import redis
 import uuid
-from typing import Union
+from typing import Optional, Union
 
 
 class Cache:
@@ -16,3 +16,23 @@ class Cache:
         random_key = str(uuid.uuid4())
         self._redis.set(random_key, data)
         return random_key
+
+    def get(self, key: str,fn: Optional[Callable] = None) -> Union[str, bytes, int, float, None]:
+        """Redis-dən məlumatı götürür və əgər 'fn' funksiyası verilibsə, məlumatı həmin funksiya ilə çevirib qaytarır."""
+        data = self._redis.get(key)
+        
+        if data is None:
+            return None
+        
+        if fn:
+            return fn(data)
+        
+        return data
+
+    def get_str(self, key: str) -> Optional[str]:
+        """converts data into string"""
+        return self.get(key, fn=lambda d: d.decode("utf-8"))
+
+    def get_int(self, key: str) -> Optional[int]:
+        """converts data to integer"""
+        return self.get(key, fn=int)
